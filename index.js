7faf2f949c81a13c1cc221bbc6cfc10267300559
Viraj R <virajransing@gmail.com>
2019-10-23 18:22:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+
+<<<<<<< HEAD
+const companyName = 'Scuber'
+
+let mostProfitableNeighborhood = "Chelsea"
+
+let companyCeo = "Susan Smith"
+=======
+const companyName = 'Scuber'
+>>>>>>> 68be3958b8a0cda81b80572418010af787755658
